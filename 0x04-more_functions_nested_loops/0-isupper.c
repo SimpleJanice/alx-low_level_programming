f98b197b3206187219de6ff792cedf605b601a81
SimpleJanice <gyeduj92@gmail.com>
2023-03-23 12:24:07 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-* _isupper - uppercase letters
+* _isupper - function that checks for upperdase character
+*
 * @c: char to check
-* Return: 0 or 1
+*
+* Return: 1 or 0
 */
-
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-
 return (1);
-else 
+else
 return (0);
 }
