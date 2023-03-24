275d3acad529dc5f0e1685dda991914ffc2e3947
SimpleJanice <gyeduj92@gmail.com>
2023-03-23 18:18:15 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,6 +7,7 @@
 * and for the multiples of five prints Buzz
 * Return: Always 0 (Success)
 */
+
 int main(void)
 {
 int i;
@@ -15,15 +16,15 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf(" Fizz");
+printf("Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf(" Buzz");
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" FizzBuzz");
+printf("FizzBuzz");
 } 
 else if (i == 1)
 {
