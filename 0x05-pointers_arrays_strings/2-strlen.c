0dc6de9f123e615ec938916d545c7aa71846a7eb
SimpleJanice <gyeduj92@gmail.com>
2023-03-27 13:04:36 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* _strlen - 
+* @str: The string to get the length of 
+* Return: The length of @str.
+*/
+int _strlen(const char *str)
+{
+size_t length = 0;
+while (*str++)
+length++;
+return (length);
+}
