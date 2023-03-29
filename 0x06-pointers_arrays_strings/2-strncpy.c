7b6bf09c37a2c3a1dbcdda44593f97fce66c51da
SimpleJanice <gyeduj92@gmail.com>
2023-03-29 15:05:13 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ while (src[index++])
 src_len++;
 for (index = 0; src[index] && index < n; index++)
 dest[index] = src[index];
-for (index =src_len; index < n; index++)
+for (index = src_len; index < n; index++)
 dest[index] = '\0';
 return (dest);
 }
