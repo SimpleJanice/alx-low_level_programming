67aadff1ed814a2c52d947b4f2a866d5cd69e9ff
SimpleJanice <gyeduj92@gmail.com>
2023-03-29 14:59:01 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,6 @@
 * @src: The source string.
 * @n: The maximum numberr of bytes to copies from src.
 * Return: A pointer to be resulting string dest.
-*
 */
 char *_strncpy(char *dest, char *src, int n)
 {
