ec87a1d696f4d5240f95496b5f5956e68e856d68
SimpleJanice <gyeduj92@gmail.com>
2023-04-05 19:21:05 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,7 +13,7 @@ if (*s1 == '\0' && *s2 == '\0')
 return (1);
 if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
-if (*s2 == '8')
+if (*s2 == '*')
 return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
 }
