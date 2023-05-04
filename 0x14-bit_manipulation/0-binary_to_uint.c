ddde296defa39cba6e5218b243b2bd1e5f8d6b12
SimpleJanice <gyeduj92@gmail.com>
2023-05-04 15:00:13 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
 * binary_to_uint - convert binary string to decimal
 * @b: binary string
@@ -21,6 +21,5 @@ decimal <<= 1;
 else if (b[i] != '0' && b[i] != '1')
 return (0);
 }
-
 return (decimal);
 }
