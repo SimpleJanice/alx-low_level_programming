abb92e39d549e68e38fc9ec340a8456f389d8f59
SimpleJanice <gyeduj92@gmail.com>
2023-08-21 16:53:59 -0700

    all

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isupper - function that checks for upperdase character
+*
+* @c: char to check
+*
+* Return: 1 or 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
