12894324480a7fd5b87d2153fe1d34b95617949b
SimpleJanice <gyeduj92@gmail.com>
2023-08-21 17:40:21 -0700

    all

--- 0x18-dynamic_libraries/1-main.c
+++ 0x18-dynamic_libraries/1-main.c
@@ -8,21 +8,21 @@ int mod(int a, int b);
 
 int add(int a, int b)
 {
-    return a + b;
+return a + b;
 }
 int sub(int a, int b)
 {
-    return a - b;
+return a - b;
 }
 int mul(int a, int b)
 {
-    return a * b;
+return a * b;
 }
 int div(int a, int b)
 {
-    return a / b;
+return a / b;
 }
 int mod(int a, int b)
 {
-    return a % b;
+return a % b;
 }
